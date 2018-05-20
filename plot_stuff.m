924a4d42d27af4ff03b6045a10b8ac2b77db11ed
HannesJubroKool <hannesjubrokool@gmail.com>
2018-05-20 17:06:24 +0200

    push

--- plot_stuff.m
+++ plot_stuff.m
@@ -12,6 +12,8 @@ acc_mean = mean(acc_data,2);
 gyr_mean = mean(gyr_data,2);
 mag_mean = mean(mag_data,2);
 
+all_mean = 
+
 acc_cov = cov(acc_data(1,:));
 acc_cov(2) = cov(acc_data(2,:));
 acc_cov(3) = cov(acc_data(3,:));
@@ -24,23 +26,18 @@ mag_cov = cov(mag_data(1,:));
 mag_cov(2) = cov(mag_data(2,:));
 mag_cov(3) = cov(mag_data(3,:));
 
-% PLOT Z-axis acceleration data histogram
+% PLOT acceleration data histogram
 figure(1)
-histogram(acc_data(3,:),'Normalization','pdf','Displayname','Z-axis acceleration data')
+axis_vec={'X','Y','Z'};
+for i = 1:3
+histogram(acc_data(i,:),'Normalization','pdf','Displayname',[axis_vec(i),'Z-axis acceleration data'])
 hold on
 x_interval = 9.8:0.001:10;
-plot(x_interval,normpdf(x_interval,acc_mean(3),sqrt(acc_cov(3))),'Displayname','Normal distribution fitted to data')
+plot(x_interval,normpdf(x_interval,acc_mean(i),sqrt(acc_cov(i))),'Displayname','Normal distribution fitted to data')
 legend('show')
+end
 
-% PLOT X-axis acceleration data histogram
-figure(2)
-histogram(acc_data(1,:),'Normalization','pdf','Displayname','X-axis acceleration data')
-hold on
-x_interval = -1:0.001:1;
-plot(x_interval,normpdf(x_interval,acc_mean(1),sqrt(acc_cov(1))),'Displayname','Normal distribution fitted to data')
-legend('show')
-
-% PLOT x-axis gyro data histogram
+% PLOTgyro data histogram
 figure(3)
 histogram(gyr_data(1,:),'Normalization','pdf','Displayname','X-axis gyro data')
 hold on
@@ -48,10 +45,13 @@ x_interval = -0.01:0.001:0.01;
 plot(x_interval,normpdf(x_interval,gyr_mean(1),sqrt(gyr_cov(1))),'Displayname','Normal distribution fitted to data')
 legend('show')
 
-% PLOT x-axis mag data histogram
+% PLOT mag data histogram
 figure(4)
 histogram(mag_data(1,:),'Normalization','pdf','Displayname','X-axis magnometer data')
 hold on
 x_interval = -40:0.001:-33;
 plot(x_interval,normpdf(x_interval,mag_mean(1),sqrt(mag_cov(1))),'Displayname','Normal distribution fitted to data')
 legend('show')
+
+
+
